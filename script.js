fda3708a2b731b3bde62d067109ebb8e7be9fa06
Fredleff <fred.leff@mykit55.com>
2026-08-28 17:31:08 -0700

    Update Weekend Send-Off accessibility label to No. 004

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ if (mediaScreen && isMediaWindowActive(mediaConfig)) {
       assetLink.href = mediaConfig.ctaUrl;
       assetLink.target = '_blank';
       assetLink.rel = 'noopener';
-      assetLink.setAttribute('aria-label', 'Open Weekend Send-Off No. 003 on LinkedIn');
+      assetLink.setAttribute('aria-label', 'Open Weekend Send-Off No. 004 on LinkedIn');
       assetLink.append(asset);
       assetContainer.replaceChildren(assetLink);
     } else {
